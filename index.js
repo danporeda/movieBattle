03bc5e845ed9577e370f510f5168f3b59644b618
Dan <danporeda@yahoo.com>
2023-12-19 23:15:48 -0500

    added dropdown component first approach

--- index.js
+++ index.js
@@ -7,25 +7,27 @@ const fetchData = async (searchTerm) => {
     }
   });
 
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  };
+  
+  return response.data.Search;
 }
 
 const input = document.querySelector('input');
 
-const debounce = (func) => {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, 1000)
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+  for (let movie of movies) {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <img src="${movie.Poster}" /> 
+      <h1>${movie.Title}</h1>
+    `;
+
+    document.querySelector('#target').appendChild(div);
   }
-}
+};
 
-const onInput = debounce(event => {
-  fetchData(event.target.value);
-});
-
-input.addEventListener('input', onInput)
+input.addEventListener('input', debounce(onInput, 500));
