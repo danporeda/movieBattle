fe5e052ed462e902184e245ffa516fddb6917625
Dan <danporeda@yahoo.com>
2023-12-24 19:15:15 -0500

    v1 of this project completed, functionl

--- index.js
+++ index.js
@@ -66,8 +66,29 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 };
 
 const runComparison = () => {
-  console.log('Time for comparison');
-}
+  const leftSideStats = document.querySelectorAll('#left-summary .notification');
+  const rightSideStats = document.querySelectorAll('#right-summary .notification');
+
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index];
+
+    // const leftSideValue = leftStat.dataset.value;
+    // const rightSideValue = rightStat.dataset.value;
+    const leftSideValue = parseFloat(leftStat.dataset.value);
+    const rightSideValue = parseFloat(rightStat.dataset.value);
+    // console.log(`${leftSideValue} > ${rightSideValue} : ${leftSideValue > rightSideValue}`);
+    // console.log(`${typeof(leftSideValue)} ${leftSideValue} > ${typeof(rightSideValue)} ${rightSideValue} : ${leftSideValue > rightSideValue}`);
+
+
+    if (rightSideValue > leftSideValue) {
+      leftStat.classList.remove('is-primary');
+      leftStat.classList.add('is-warning');
+    } else if (rightSideValue < leftSideValue) {
+      rightStat.classList.remove('is-primary');
+      rightStat.classList.add('is-warning');
+    }
+  });
+};
 
 const movieTemplate = (movieDetail) => {
   const dollars = parseInt(
@@ -85,7 +106,6 @@ const movieTemplate = (movieDetail) => {
       return prev + value;
     }
   }, 0);
-  console.log(awards);
 
   return `
   <article class="media">
@@ -102,23 +122,23 @@ const movieTemplate = (movieDetail) => {
       </div>
     </div>
   </article>
-  <article class="notification is-primary">
+  <article data-value="${awards}" class="notification is-primary">
     <p class="title">${movieDetail.Awards}</p>
     <p class="subtitle">Awards</p>
   </article>
-  <article class="notification is-primary">
+  <article data-value="${dollars}" class="notification is-primary">
     <p class="title">${movieDetail.BoxOffice}</p>
     <p class="subtitle">Box Office</p>
   </article>
-  <article class="notification is-primary">
+  <article data-value="${metascore}" class="notification is-primary">
     <p class="title">${movieDetail.Metascore}</p>
     <p class="subtitle">Metascore</p>
   </article>
-  <article class="notification is-primary">
+  <article data-value="${imdbRating}" class="notification is-primary">
     <p class="title">${movieDetail.imdbRating}</p>
     <p class="subtitle">IMDB Rating</p>
   </article>
-  <article class="notification is-primary">
+  <article data-value="${imdbVotes}" class="notification is-primary">
     <p class="title">${movieDetail.imdbVotes}</p>
     <p class="subtitle">IMDB Votes</p>
   </article>
